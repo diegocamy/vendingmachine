7094f5de58e39a5ba81506102a449ef65b9a2237
diegocamy <53247032+diegocamy@users.noreply.github.com>
2019-12-19 00:10:29 -0300

    Updates

---
+++ precache-manifest.f58478db642f5f0038dcba6b3bae0fe6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f9fb92430b07de10adf3f4259a5493",
+    "url": "/vendingmachine/index.html"
+  },
+  {
+    "revision": "cffa49c63d5153f4f5be",
+    "url": "/vendingmachine/static/css/main.e17a673a.chunk.css"
+  },
+  {
+    "revision": "26df165011dbbcc19221",
+    "url": "/vendingmachine/static/js/2.e21b800e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/vendingmachine/static/js/2.e21b800e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cffa49c63d5153f4f5be",
+    "url": "/vendingmachine/static/js/main.b573bbe3.chunk.js"
+  },
+  {
+    "revision": "232aceda277a13fc695e",
+    "url": "/vendingmachine/static/js/runtime-main.b5d844ca.js"
+  },
+  {
+    "revision": "dd375c3fd70e73d0f3ad2b8e6ee2d6c7",
+    "url": "/vendingmachine/static/media/1.dd375c3f.png"
+  },
+  {
+    "revision": "bff011a078bcb67054d5e5ef834a5437",
+    "url": "/vendingmachine/static/media/2.bff011a0.png"
+  },
+  {
+    "revision": "cedb28240ec4bd301fb66975875dacc9",
+    "url": "/vendingmachine/static/media/3.cedb2824.png"
+  },
+  {
+    "revision": "bd0cfc6fa96be4cb3f94d06d6badbe6a",
+    "url": "/vendingmachine/static/media/4.bd0cfc6f.png"
+  },
+  {
+    "revision": "b4a417f549c998ba116741edf82bae40",
+    "url": "/vendingmachine/static/media/coca.b4a417f5.png"
+  },
+  {
+    "revision": "35e3b7857975c39f2ffee8d76bc64f5c",
+    "url": "/vendingmachine/static/media/vending.35e3b785.jpg"
+  }
+]);
